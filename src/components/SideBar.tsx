9931292c57651f62955cc3cac1853065ee841f13
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-06-30 03:55:12 -0300

    refactor: componentização do header e content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,45 @@
-export function SideBar() {
-  // Complete aqui
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { Button } from '../components/Button';
+import { api } from '../services/api';
+import { GenreResponseProps } from '../App';
+
+import '../styles/sidebar.scss';
+
+type SideBarProps = {
+  selectedGenreId: number;
+  setSelectedGenreId: Dispatch<SetStateAction<number>>;
+};
+
+export function SideBar({ selectedGenreId, setSelectedGenreId }: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
